aabc564e3a147f7eaf596a0b507799a62993c3e4
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-13 17:37:27 +0530

    welcome to stcak operation

--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class StackOperations
+class QueueUsingLinkedList<T>
+{
+    private LinkedList<T> list = new LinkedList<T>();
+
+    public void Enqueue(T value)
+    {
+        // Enqueue the value at the end of the queue
+        list.AddLast(value);
+    }
+
+    public T Dequeue()
+    {
+        if (list.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        T value = list.First.Value;
+        list.RemoveFirst();
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (list.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        return list.First.Value;
+    }
+
+    public bool IsEmpty()
+    {
+        return list.Count == 0;
+    }
+}
+
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Stack Operations using LinkedList in C#");
+        QueueUsingLinkedList<int> queue = new QueueUsingLinkedList<int>();
 
-        Stack<int> myStack = new Stack<int>();
+        // Enqueue values into the queue
+        queue.Enqueue(56);
+        queue.Enqueue(30);
+        queue.Enqueue(70);
 
-        myStack.Push(56);
-        myStack.Push(30);
-        myStack.Push(70);
-
-        Console.WriteLine("Peek: " + myStack.Peek()); // Peek without removing
-        Console.WriteLine("Popping elements:");
-
-        while (myStack.Count > 0)
+        // Dequeue and peek until the queue is empty
+        while (!queue.IsEmpty())
         {
-            Console.WriteLine(myStack.Pop()); // Pop and remove
+            Console.WriteLine("Front of the queue: " + queue.Peek());
+            Console.WriteLine("Dequeued value: " + queue.Dequeue());
         }
     }
 }
