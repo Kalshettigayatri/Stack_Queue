a70d58d279661a7639dbb4dcdb9410d9746cd827
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-13 17:34:02 +0530

    pop operation stack

--- Program.cs
+++ Program.cs
@@ -1,54 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class StackUsingLinkedList<T>
-{
-    private LinkedList<T> list = new LinkedList<T>();
-
-    public void Push(T value)
-    {
-        // Push the value onto the top of the stack
-        list.AddLast(value);
-    }
-
-    public T Pop()
-    {
-        if (list.Count == 0)
-        {
-            throw new InvalidOperationException("Stack is empty");
-        }
-
-        T value = list.Last.Value;
-        list.RemoveLast();
-        return value;
-    }
-
-    public T Peek()
-    {
-        if (list.Count == 0)
-        {
-            throw new InvalidOperationException("Stack is empty");
-        }
-
-        return list.Last.Value;
-    }
-}
-
-class Program
+class StackOperations
 {
     static void Main(string[] args)
     {
-        StackUsingLinkedList<int> stack = new StackUsingLinkedList<int>();
+        Console.WriteLine("Stack Operations using LinkedList in C#");
 
-        // Pushing values onto the stack
-        stack.Push(70);
-        stack.Push(30);
-        stack.Push(56);
+        Stack<int> myStack = new Stack<int>();
 
-        // Peeking and popping values from the stack
-        Console.WriteLine("Top of the stack: " + stack.Peek()); // Should print 56
-        Console.WriteLine("Popped value: " + stack.Pop());     // Should print 56
-        Console.WriteLine("Popped value: " + stack.Pop());     // Should print 30
-        Console.WriteLine("Top of the stack: " + stack.Peek()); // Should print 70
+        myStack.Push(56);
+        myStack.Push(30);
+        myStack.Push(70);
+
+        Console.WriteLine("Peek: " + myStack.Peek()); // Peek without removing
+        Console.WriteLine("Popping elements:");
+
+        while (myStack.Count > 0)
+        {
+            Console.WriteLine(myStack.Pop()); // Pop and remove
+        }
     }
 }
